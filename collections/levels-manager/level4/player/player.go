5d1c5e29087e53350088d7f1b88b205df7289ddf
Rubentxu <rubentxudev@gmail.com>
2022-05-02 16:11:55 +0200

    dev: mejoras level trayectorias

---
+++ collections/levels-manager/level4/player/player.go
@@ -0,0 +1,71 @@
+components {
+  id: "player"
+  component: "/scripts/level4/player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/collections/levels-manager/level4/level4.atlas\"\n"
+  "default_animation: \"TEX_Dot_Hard\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/collections/levels-manager/level4/player/dot.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bulletFactory"
+  type: "factory"
+  data: "prototype: \"/collections/levels-manager/level4/player/bullet.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
